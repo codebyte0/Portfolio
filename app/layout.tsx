88b0e8fa41745048316eafcef14daf007e596ca8
codebyte0 <codebyte.wd@gmail.com>
2024-10-03 23:02:35 -0700

    4Octlast

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "./provider";
 import dynamic from 'next/dynamic';
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-import Script from "next/script"; // Import Script component from next/script
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,22 +23,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} suppressHydrationWarning={true}>
-        {/* Google Analytics Script */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-XV9LX8EKFC"
-        />
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-XV9LX8EKFC"></Script>
         <Script id="google-analytics">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'G-XV9LX8EKFC');
           `}
         </Script>
-
+      </head>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <ThemeToggle />
         <ThemeProvider
           attribute="class"
